d6f14b1cc7ae6528f48047a19503ff2fbea9ae33
Blessing Peters <cherubpeters@gmail.com>
2026-04-28 22:14:49 +0100

    Add stage zero project

---
+++ simple_code.py
@@ -0,0 +1,7 @@
+name = "Blessing Peter"
+affiliation = "University of Uyo"
+favorite_gene = "HER2"
+organism = "Rattus norvegicus (Wistar rat)"
+
+print(f"Hi, my name is {name}, a researcher at the {affiliation}. "
+      f"My favorite gene is {favorite_gene} in {organism}.")
